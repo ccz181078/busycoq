6ddf7214fda126120fc08fe9acdd33719ff2277e
ccz181078 <1794513099@qq.com>
2025-02-07 03:06:25 +0800

    add comments in Example.v

--- verify/Example.v
+++ verify/Example.v
@@ -9,28 +9,42 @@ Module TC_Example1.
 
 Definition tm := (TM_from_str "1RB---_0RA---_------_------_------_------").
 
+(*
+  a -->* b means b is reachable from a by >=0 steps of TM execution
+  a -->+ b means b is reachable from a by >0 steps of TM execution
+ *)
 Notation "c -->* c'" := (c -[ tm ]->* c') (at level 40).
 Notation "c -->+ c'" := (c -[ tm ]->+ c') (at level 40).
 
+(*
+  S0(l) is defined as l A> 0^inf
+ *)
 Definition S0 (l:side) := l {{A}}> const 0.
 
+(*
+  l A> 0^inf -->+ l 10 A> 0^inf
+ *)
 Lemma Inc l:
   S0 l -->+ S0 (l <* <[1;0]).
 Proof.
-  es.
+  es. (* trivial TM execution *)
 Qed.
 
+(*
+  0^inf A> 0^inf -->* 0^inf A> 0^inf
+ *)
 Lemma init:
   c0 -->* S0 (const 0).
 Proof.
   es.
 Qed.
 
+(* prove nonhalt staring from 0^inf A> 0^inf *)
 Lemma nonhalt: ~halts tm c0.
 Proof.
-  eapply multistep_nonhalt.
+  eapply multistep_nonhalt. (* to prove A nonhalt, we can prove B-->*A and B nonhalt *)
   1: apply init.
-  eapply progress_nonhalt_simple.
+  eapply progress_nonhalt_simple. (* to prove f(x) nonhalt, we can prove forall x, exists y, f(x)-->+f(y) *)
   intros i.
   eexists.
   apply Inc.
@@ -46,12 +60,15 @@ Definition tm := (TM_from_str "1LB1RA_1RA1LB_------_------_------_------").
 Notation "c -->* c'" := (c -[ tm ]->* c') (at level 40).
 Notation "c -->+ c'" := (c -[ tm ]->+ c') (at level 40).
 
+(*
+  S0(n) := 0^inf 1^n A> 0^inf
+ *)
 Definition S0 n := const 0 <* [1]^^n {{A}}> const 0.
 
 Lemma Inc n:
   S0 n -->+ S0 (2+n).
 Proof.
-  es.
+  es. (* es also support executing shift rule *)
 Qed.
 
 Lemma init:
@@ -87,11 +104,11 @@ Lemma S0_to_S1 l n:
   S0 l n -->* S1 l n.
 Proof.
   gen l.
-  induction n; intros.
+  induction n; intros. (* prove by induction on n *)
   - es.
-  - mid (S0 (l<*[0]) n).
+  - mid (S0 (l<*[0]) n). (* we can prove A-->*C by specifying B and then prove A-->*B and B-->*C *)
     1: es.
-    follow IHn.
+    follow IHn. (* we can prove B-->*C by specifying a rule B-->*D and then prove D-->*C *)
     mid (S0 (l<*[1]) n).
     1: es.
     follow IHn.
@@ -102,7 +119,7 @@ Lemma Inc n:
   S1 (const 0) n -->+
   S1 (const 0) (1+n).
 Proof.
-  mid10 (S0 (const 0<*[1]) n).
+  mid10 (S0 (const 0<*[1]) n). (* similar to mid, but split A-->+C as A-->+B and B-->*C *)
   1: es.
   follow S0_to_S1.
   es.
@@ -148,7 +165,7 @@ Lemma Incs0 a b:
   S0 (b+a) 0.
 Proof.
   gen a.
-  ind b Inc0.
+  ind b Inc0. (* prove by induction on b, and Inc0 is one induction step *)
 Qed.
 
 Lemma Inc1 n:
